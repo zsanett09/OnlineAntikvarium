74bacb6bc2c7b34beb92848a61a2487829c50e44
Orosz Zsanett <oroszzsanett@gmail.com>
2014-03-30 16:16:26 +0200

    proba

---
+++ Antikvarium.java
@@ -0,0 +1,11 @@
+public class Antikvarium {
+
+	private HirdetesKezelo hirdetesKezelo;
+	private Felhasznalok felhasznalok;
+
+	public  Antikvarium() {
+		// TODO - implement Antikvarium.Antikvarium
+		
+	}
+
+}
